e8cc02c2b42430172562ad6c5599d8773a699ef3
Abdallah Meghraoui <abdallah.meghraoui@grenoble-ino.org>
2023-10-29 00:53:08 +0200

    feat: header with some animation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'frontend';
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    AOS.init({
+      duration: 1000
+    });
+  }
 }
--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-      window.addEventListener('scroll', this.onScroll);
+    window.addEventListener('scroll', this.onScroll);
   }
 
   onScroll = () => {
