1a8ed12b906145248123c931a8f41ccb3314dc95
saidabde <abdennacer.saidi@grenoble-inp.fr>
2023-10-30 11:27:03 +0100

    feat/add person overview and animation

---
+++ frontend/src/app/components/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HomeComponent} from "./home.component";
+
+
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule
+  ],
+  bootstrap: [HomeComponent]
+})
+export class HomeModule { }
