a431cb594abb66c9000a20a1aebfa7f8643ea650
Abdallah Meghraoui <abdallah.meghraoui@grenoble-ino.org>
2023-10-30 01:09:20 +0100

    refacto: init languages

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LanguageService } from "src/app/services/language/language.service"
+import { TranslateService } from '@ngx-translate/core';
 import * as AOS from 'aos';
 
 @Component({
@@ -11,11 +11,12 @@ export class AppComponent implements OnInit {
   title = 'frontend';
 
   constructor(
-    private languageService: LanguageService
+    private translateService: TranslateService
   ) { }
 
   ngOnInit(): void {
-    this.languageService.initLanguage()
+    this.translateService.addLangs(["fr", "en"])
+    this.translateService.setDefaultLang("en")
     AOS.init({
       duration: 1000
     });
