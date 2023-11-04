709150153e4367679353e0f327ac1aa438af8779
saidabde <abdennacer.saidi@grenoble-inp.fr>
2023-11-04 03:21:25 +0100

    feat/Add the scroll down mouse icon

---
+++ frontend/src/app/components/home/mouse/mouse.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mouse',
+  templateUrl: './mouse.component.html',
+  styleUrls: ['./mouse.component.scss']
+})
+export class MouseComponent {
+
+}
