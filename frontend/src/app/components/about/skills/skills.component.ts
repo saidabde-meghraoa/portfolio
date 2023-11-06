f68f0470a2cea07d55a770d0502505fa7cf87499
saidabde <abdennacer.saidi@grenoble-inp.fr>
2023-11-06 12:12:46 +0100

    feat/Add the footer of the page and some links interesting links

--- frontend/src/app/components/about/skills/skills.component.ts
+++ frontend/src/app/components/about/skills/skills.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {faGit, faCodepen, faFreeCodeCamp, faMeetup, faHtml5, faCss3, faWindows, faLinux, faEdge, faApple, faChrome, faAndroid, faFirefox} from "@fortawesome/free-brands-svg-icons";
-import {faCoffee, faTerminal, faHeart, faCode} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-skills',
@@ -9,21 +7,4 @@ import {faCoffee, faTerminal, faHeart, faCode} from "@fortawesome/free-solid-svg
 })
 export class SkillsComponent {
 
-  protected readonly faGit = faGit;
-  protected readonly faMeetup = faMeetup;
-  protected readonly faFreeCodeCamp = faFreeCodeCamp;
-  protected readonly faCodepen = faCodepen;
-  protected readonly faHtml5 = faHtml5;
-  protected readonly faCss3 = faCss3;
-  protected readonly faWindows = faWindows;
-  protected readonly faEdge = faEdge;
-  protected readonly faLinux = faLinux;
-  protected readonly faApple = faApple;
-  protected readonly faChrome = faChrome;
-  protected readonly faAndroid = faAndroid;
-  protected readonly faFirefox = faFirefox;
-  protected readonly faCoffee = faCoffee;
-  protected readonly faTerminal = faTerminal;
-  protected readonly faHeart = faHeart;
-  protected readonly faCode = faCode;
 }
